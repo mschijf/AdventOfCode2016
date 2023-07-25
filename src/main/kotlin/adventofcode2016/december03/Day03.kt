81467ed90d305d9e30987acf59c9aab83d00ab17
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-07-25 07:48:59 +0200

    Day 3 - bug fix

--- src/main/kotlin/adventofcode2016/december03/Day03.kt
+++ src/main/kotlin/adventofcode2016/december03/Day03.kt
@@ -24,7 +24,7 @@ class Day03(test: Boolean) : PuzzleSolverAbstract(test) {
             .map { line -> line.trim().split("\\s+".toRegex()).map { it.toInt() } }
 
         return with(input) {
-            (0..input.first().size).map { i -> map { it[i] } } //transposing over 3 columns -> 3 lists
+            (0..< input.first().size).map { i -> map { it[i] } } //transposing over 3 columns -> 3 lists
                 .flatten()//one big list
                 .chunked(3)  //list of lists of 3 elements
                 .map { it.sorted() } // sort the small lists
