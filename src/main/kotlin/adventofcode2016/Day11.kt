4f33426afa4c213ee3e131d1dfeece263b493f3e
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-08-01 07:55:09 +0200

    Day 11 - finally solution, need to be refactored and made more performing

--- src/main/kotlin/adventofcode2016/Day11.kt
+++ src/main/kotlin/adventofcode2016/Day11.kt
@@ -58,9 +58,9 @@ class Day11(test: Boolean) : PuzzleSolverAbstract(test) {
             building
                 .elevatorCandidates()
                 .map { move -> building.doMove(move) }
-                .filter { building -> building.hashKey() !in alreadySeen }.forEach { building ->
-                    alreadySeen += building.hashKey()
-                    deque.addLast(Pair(building, stepsDone + 1))
+                .filter { nextBuilding -> nextBuilding.hashKey() !in alreadySeen }.forEach { nextBuilding ->
+                    alreadySeen += nextBuilding.hashKey()
+                    deque.addLast(Pair(nextBuilding, stepsDone + 1))
                 }
         }
         println("nodes visited: $count")
@@ -84,7 +84,20 @@ class Building(private val floors: List<Set<Component>>, private val elevatorPos
     override fun toString(): String {
         return "E$elevatorPos | ${floorString(0)} | ${floorString(1)} | ${floorString(2)} | ${floorString(3)}"
     }
+    private fun floorString(nr: Int) = floors[nr].sortedBy { it.name }.joinToString(",")
 
+
+    /**
+     * hashkey bepaalt voor een configuratie in een building de 'waarde' rekeninghoudend met syoniemen.
+     *         hoe zijn de chips, generators en elevator positie verdeeld over de verdiepingen
+     *
+     * Het maakt namelijk niet uit om het aantal stappen tot de top te bepalen voor onderstaande synoniemen
+     *     (Ga@vloer0, Ca@vloer0), (Gb@vloer1, Cb@vloer1),
+     *     (Gb@vloer0, Cb@vloer0), (Ga@vloer1, Ca@vloer1)
+     *
+     * Sterker nog, het enige dat uitmaakt (uitgaande van een legale stelling) is
+     *    het aantal generators en aantal chips per vloer (deze snap ik nog niet --> overgenomen van internet)
+     */
     fun hashKey(): String {
 //        return "$elevatorPos|${floors.hashCode()}"
         val indexer = mutableMapOf<Char, Int>()
@@ -97,23 +110,14 @@ class Building(private val floors: List<Set<Component>>, private val elevatorPos
                 }
             }
         }
-        val maxI = i
-        val floorMax = maxI * 8
-        val sumList = floors.mapIndexed { floorIndex, floor ->
-            floor.sumOf { component ->  indexer[component.name[0]]!! + if (component is Chip) 0 else maxI }
+        val generatorBit = i
+        val sumList = floors.map { floor ->
+            floor.sumOf { component ->  indexer[component.name[0]]!! or if (component is Chip) 0 else generatorBit }
         }
+//        val sumList = floors.map { floor -> Pair(floor.count { it is Chip }, floor.count{it is Generator}) }
         val total = sumList.joinToString("-")
-
-//        var total = 0
-//        floors.forEachIndexed { floorIndex, floor ->
-//            total += floorMax * floorIndex +
-//                    floor.sumOf { component -> indexer[component.name[0]]!! + if (component is Chip) 0 else maxI }
-//        }
-
         return "$elevatorPos|$total"
     }
-
-    private fun floorString(nr: Int) = floors[nr].sortedBy { it.name }.joinToString(",")
 
     fun doMove(move: Move): Building {
         return Building(
@@ -132,10 +136,11 @@ class Building(private val floors: List<Set<Component>>, private val elevatorPos
     fun elevatorCandidates(): List<Move> {
         val currentFloor = floors[elevatorPos]
         val allCandidates =
-            currentFloor.toList().mapCombinedItems { comp1, comp2 -> listOf(comp1, comp2) } +
-                    currentFloor.map { comp -> listOf(comp) } //+ listOf(emptyList<Component>()) +
+            currentFloor.toList()
+                .mapCombinedItems { comp1, comp2 -> listOf(comp1, comp2) } + currentFloor.map { comp -> listOf(comp) }
+                .filter {candidate -> (currentFloor - candidate).isSecure() }
 
-        return (-1..1 step 2).filter { elevatorPos+it in 0..3 }
+        return (-1..1 step 2).filter { deltaPos -> elevatorPos + deltaPos in 0..3 }
             .flatMap{ deltaPos -> allCandidates.validForFloor(floors[elevatorPos+deltaPos]).map{validCandidate -> Move(deltaPos, validCandidate) }}
     }
 
