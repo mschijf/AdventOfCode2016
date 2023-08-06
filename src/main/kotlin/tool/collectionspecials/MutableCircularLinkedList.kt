c14797408fe14da18da71b0ca1cb704bbb3b7164
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-08-06 09:59:13 +0200

    Refactored CircularList - made it subclass from MutableCollection - note: the 'index' is a strangeConcept

--- src/main/kotlin/tool/collectionspecials/MutableCircularLinkedList.kt
+++ src/main/kotlin/tool/collectionspecials/MutableCircularLinkedList.kt
@@ -26,27 +26,23 @@ fun <T> Iterable<T>.toMutableCircularLinkedList(): MutableCircularLinkedList<T> 
     return cll
 }
 
-class MutableCircularLinkedList<T>: Collection<T> {
-    private val cllId = Random.nextInt().absoluteValue
+class MutableCircularLinkedList<T>: MutableCollection<T> {
+    private var cllId: Int = 0
     private var first: Node? = null
-    fun firstIndexOrNull(): ListIndex? = first
-    fun firstIndex(): ListIndex = if (first != null) first!! else throw Exception("Circular list is Empty")
 
     override var size = 0
         private set
 
-    override fun isEmpty(): Boolean {
-        return size == 0
-    }
+    init { clear() }
 
-    override fun containsAll(elements: Collection<T>) =
-        this.toSet().containsAll(elements)
+    fun firstIndexOrNull(): ListIndex? =
+        first
 
-    override fun contains(element: T) =
-        this.any { elt -> element == elt}
+    fun firstIndex(): ListIndex =
+        if (first != null) first!! else throw Exception("Circular list is Empty")
 
-    fun isNotEmpty() =
-        !isEmpty()
+    override fun isEmpty() =
+        size == 0
 
     operator fun get(listIndex: ListIndex) =
         listIndex.asNode().data
@@ -58,33 +54,24 @@ class MutableCircularLinkedList<T>: Collection<T> {
         return prevElement
     }
 
-    /**
-     * add an element at the end of the list, which is the same as before the first element ever inserted
-     *
-     * if list is empty, then it will be the first element
-     *
-     * returns: the node including the element
-     */
-    fun add(element: T) {
-        if (first == null) {
-            addFirst(element)
-        } else {
-            add(firstIndexOrNull()!!, element)
-        }
-    }
+    override fun contains(element: T) =
+        this.any { elt -> element == elt}
 
-    private fun addFirst(element: T) {
-        size++
+    override fun add(element: T) =
+        if (first == null) addFirst(element) else add(firstIndex(), element)
+
+    private fun addFirst(element: T): Boolean {
         first = newNode(element, null, null)
+        size++
+        return true
     }
-
 
     /**
      * Inserts an element into the list at the specified listIndex
      * assumption: the listIndex exists
      *
      */
-    fun add(listIndex: ListIndex, element: T) {
+    fun add(listIndex: ListIndex, element: T): Boolean {
         val node = listIndex.asNode()
 
         val new = newNode(element, node.prev, node)
@@ -93,11 +80,11 @@ class MutableCircularLinkedList<T>: Collection<T> {
         tmpPrev.next = new
         tmpNext.prev = new
         size++
+        return true
     }
 
     /**
      * removes the element at 'listIndex'
-     * assumption: the listIndex exists
      *
      * returns the data that was on this listIndex
      */
@@ -106,7 +93,7 @@ class MutableCircularLinkedList<T>: Collection<T> {
 
         node.prev.next = node.next
         node.next.prev = node.prev
-        if (first == node) {
+        if (node == first) {
             first = node.next
         }
         size--
@@ -120,22 +107,94 @@ class MutableCircularLinkedList<T>: Collection<T> {
         return node.data
     }
 
-    override fun toString() =
-        this.joinToString(" ")
+    override fun remove(element: T): Boolean {
+        val tobeRemoved = firstIndexOfOrNull(element)
+        if (tobeRemoved != null) {
+            removeAt(tobeRemoved)
+            return true
+        }
+        return false
+    }
 
-    override fun iterator(): Iterator<T>  =
-        CircularLinkedListIterator(this)
+    fun firstIndexOfOrNull(element: T) : ListIndex? {
+        if (isEmpty())
+            return null
+        if (this[firstIndex()] == element)
+            return firstIndex()
+
+        var walker = firstIndex() + 1
+        while (walker != first && this[walker] != element)
+            walker++
+        return walker
+    }
 
     private fun newNode(data: T, pprev: Node?, pnext: Node?) =
         Node(data, pprev, pnext, cllId)
 
     private fun ListIndex.asNode() : Node {
         val node = this as MutableCircularLinkedList<T>.Node
-        if (node.cllId != cllId) {
+        if (node.cllId != this@MutableCircularLinkedList.cllId) {
             throw Exception ("List Index not belonging to (Circular)LinkedList")
         }
         return node
     }
+
+    override fun toString() =
+        "[${this.joinToString(", ")}]"
+
+    override fun iterator(): MutableIterator<T> =
+        CircularLinkedListIterator(this)
+
+    override fun retainAll(elements: Collection<T>): Boolean {
+        var result = false
+        if (isEmpty())
+            return false
+        val retainElements = elements.toSet()
+
+        var walker = firstIndex() + 1
+        while (walker != firstIndex()) {
+            if (this[walker] in retainElements) {
+                walker++
+            } else {
+                val toBeRemoved = walker
+                walker++
+                removeAt(toBeRemoved)
+                result = true
+            }
+        }
+
+        if (this[firstIndex()] !in retainElements) {
+            removeAt(firstIndex())
+            result = true
+        }
+
+        return result
+    }
+
+    override fun removeAll(elements: Collection<T>): Boolean {
+        var atLeastOneRemoved = false
+        elements.forEach { element ->
+            val removed = remove(element)
+            atLeastOneRemoved = atLeastOneRemoved || removed
+        }
+        return atLeastOneRemoved
+    }
+
+    override fun clear() {
+        size = 0
+        first = null
+        cllId = Random.nextInt().absoluteValue
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        elements.forEach { element -> add(element) }
+        return true
+    }
+
+    override fun containsAll(elements: Collection<T>) =
+        this.toSet().isNotEmpty()
+
+    //==================================================================================================================
 
     private inner class Node(var data: T, pprev: Node?, pnext: Node?, var cllId: Int): ListIndex {
         var prev: Node = pprev ?: this
@@ -172,25 +231,32 @@ class MutableCircularLinkedList<T>: Collection<T> {
 
     }
 
-    inner class CircularLinkedListIterator(private val cll: MutableCircularLinkedList<T>): Iterator<T> {
-        private var current = firstIndexOrNull()
-        private var neverIterated = true
+    //==================================================================================================================
 
-        override fun hasNext(): Boolean {
-            if (cll.isEmpty())
-                return false
-            if (neverIterated)
-                return true
-            return current !== cll.firstIndexOrNull()
-        }
+    inner class CircularLinkedListIterator(private val cll: MutableCircularLinkedList<T>): MutableIterator<T> {
+        private var cursor:ListIndex? = null
+        private var lastReturned:ListIndex? = null
+
+        override fun hasNext() =
+            (cll.size > 0) && (cursor == null || cursor !== cll.firstIndexOrNull())
 
         override fun next(): T {
             if (!hasNext())
-                throw Exception("No next on CycledLinkedList iterator")
-            neverIterated = false
-            val data = cll[current!!]
-            current = current!! + 1
+                throw NoSuchElementException()
+            if (cursor == null)
+                cursor = cll.firstIndexOrNull()
+            val data = cll[cursor!!]
+            lastReturned = cursor
+            cursor = cursor!! + 1
             return data
+        }
+
+        override fun remove() {
+            check(lastReturned != null)
+            if (lastReturned == firstIndexOrNull())
+                cursor = null
+            cll.removeAt(lastReturned!!)
+            lastReturned = null
         }
     }
 }
